fa9fce54ae56aa0dbac2644461d2d2286070962d
Alexander <spindelman2000@gmail.com>
2021-04-29 13:45:06 +0100

    k
    k

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,24 +6,42 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int health;
-    public Text healthText;
+    [SerializeField] private int health;
+    [SerializeField] private Text healthText;
 
     private void Start()
     {
         healthText.text = health.ToString();
     }
+	
+	void FixedUpdate()
+	{
+		HealthState();
+	}
+	
+	public int GetHealth()
+	{
+		return health;
+	}
+	
+	public void SetHealth(int setValue)
+	{
+		health = setValue;
+	}
 
     public void Damage(int damage)
     {
         health -= damage;
 
-        if(health <= 0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-
         healthText.text = health.ToString();
         Debug.Log("Player: Health Left = " + health);
     }
+	
+	private void HealthState()
+	{
+		if(health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
 }
---
+++ Assets/Scripts/Player/customPlayerController3D.cs
@@ -0,0 +1,109 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customPlayerController3D : MonoBehaviour
+{
+	
+	//Somnium
+	[SerializeField] private float speed;
+	[SerializeField] private float jumpSpeed;
+	[SerializeField] private float mouseSensitivity;
+	[SerializeField] private float deathHeight;
+	[SerializeField] private float backingSpeedFactor;
+	[SerializeField] private float strafingSpeedFactor;
+
+    void Awake()
+    {
+	}
+    void FixedUpdate()
+    {
+		DeathFall();
+    }
+	
+	
+	public float GetMouseSensitivity()
+	{
+		float output = mouseSensitivity;
+		return output;
+	}
+	
+	public Vector3 GetInputMove()
+	{
+		bool grounded = !GetComponent<customPhysics3D>().GetAirbourne();
+
+		float xDirection = Input.GetAxisRaw("Horizontal");
+		float zDirection = Input.GetAxisRaw("Vertical");
+		bool yInput = false;
+		
+		if(grounded)
+		{
+			yInput = Input.GetButton/*Down*/("Jump");
+		}
+		else
+		{
+			yInput = false;
+		}
+		
+		Vector3 velocity = Jump(yInput) + XMovement(xDirection) + ZMovement(zDirection);
+		Vector3 inputMovement = transform.rotation * velocity;
+		
+		
+		Vector3 output = inputMovement/*.normalized*/;
+		return output;
+	}
+	
+	private Vector3 Jump(bool input)
+	{
+		Vector3 jumpVector = Vector3.zero;
+		
+		if(input)
+		{
+			jumpVector = new Vector3(0,jumpSpeed,0);
+		}
+		else
+		{}
+		
+		Vector3 output = jumpVector;
+		return output;
+	}
+	
+	private Vector3 XMovement(float direction)
+	{
+		Vector3 xVector = new Vector3(speed,0,0);
+		xVector = xVector * direction;
+		
+		if(xVector.x != 0)
+		{
+			xVector = xVector * strafingSpeedFactor;
+		}
+		
+		Vector3 output = xVector;
+		return output;
+	}
+	
+	private Vector3 ZMovement(float direction)
+	{
+		Vector3 zVector = new Vector3(0,0,speed);
+		zVector = zVector * direction;
+		
+		if(zVector.z < 0)
+		{
+			zVector = zVector * backingSpeedFactor;
+		}
+		
+		Vector3 output = zVector;
+		return output;
+	}
+	
+	private void DeathFall()
+	{
+		if(transform.position.y <= deathHeight)
+		{
+			//Destroy(gameObject);
+			GetComponent<PlayerHealth>().SetHealth(0);
+			print("ded?");
+		}
+	}
+}
